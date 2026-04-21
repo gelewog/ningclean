1f76c96daaa17e4ef8ac1e3a87f66a24122e9d01
gelewog <gelewog@gmail.com>
2026-04-21 13:39:04 +0700

    cleanup: remove temporary and debug files
    - Remove article-*.txt temp files
    - Remove check-*.js, test-*.js, verify-*.js debug scripts
    - Remove create-article-*.js one-time scripts
    - Remove update-*.js/sql migration scripts
    - Remove backup-*.js, simulate-*.js temp scripts
    - Remove scripts-cek-db/ folder
    - Remove dto/ folder (root)
    - Remove animate-clean-*.png images
    - Remove apps/api/check-db.js, update-*.js
    - Update .gitignore to prevent future commits of temp files

--- check baileys.js
+++
@@ -1,6 +0,0 @@
-const baileys = require('baileys');
-console.log('Available exports:', Object.keys(baileys).slice(0, 30));
-// Check for event types
-if (baileys.EventEmitter) {
-  console.log('Has EventEmitter');
-}
--- check-bailleys2.js
+++
@@ -1,9 +0,0 @@
-const baileys = require('baileys');
-console.log('All keys:', Object.keys(baileys));
-console.log('EventEmitter type:', typeof baileys.EventEmitter);
-console.log('makeWASocket type:', typeof baileys.makeWASocket);
-
-// Check for BaileysEventMap
-if (baileys.BaileysEventMap) {
-  console.log('BaileysEventMap:', Object.keys(baileys.BaileysEventMap));
-}
